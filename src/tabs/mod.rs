286ab01b0726cb3bb7bf795411256563aec24c5c
Zach <zachcshore@gmail.com>
2023-08-13 11:02:21 -0500

    font update

--- src/tabs/mod.rs
+++ src/tabs/mod.rs
@@ -2,9 +2,10 @@ mod overview;
 mod repositories;
 mod projects;
 mod packages;
+mod notfound;
 
 pub use overview::*;
 pub use packages::*;
 pub use projects::*;
 pub use repositories::*;
-
+pub use notfound::*;
